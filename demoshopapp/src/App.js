5c9349f59ac07d756ecc0c43f2901379d5056e9f
Gourav01s <gourav8891s@gmail.com>
2025-11-15 15:11:11 +0530

    initial commit

---
+++ demoshopapp/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Item from './components/Item';
+
+function App() {
+  return (
+    <div className='main'>
+
+      <Item></Item>
+      <div className="App">
+        hello there
+      </div>
+      
+    </div >
+  );
+}
+
+export default App;
---
+++ demoshopapp/src/components/Item.js
@@ -0,0 +1,7 @@
+import './Item.css';
+
+function Item() {
+    return (<p className='newitem'> paragraph from Item component</p>)
+}
+
+export default Item;
