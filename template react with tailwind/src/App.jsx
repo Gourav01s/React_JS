882888298d0d500d581210b09bff01676b161b00
Gourav01s <gourav8891s@gmail.com>
2025-11-15 14:47:08 +0530

    inital commit

---
+++ template react with tailwind/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <h1>hello project</h1>
+        <h1 className="text-3xl font-bold underline bg-red-500">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
